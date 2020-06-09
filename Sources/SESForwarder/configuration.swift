c1fee60fd7dc34da76c3fa7466c19f37aa1b5007
Adam Fowler <adamfowler71@gmail.com>
2020-06-09 15:11:32 +0100

    Update configuration
    It now includes more examples of forward mapping

--- Sources/SESForwarder/configuration.swift
+++ Sources/SESForwarder/configuration.swift
@@ -4,7 +4,9 @@ extension SESForwarderHandler {
         static let fromAddress: String = "noreply@example.com"
         static let subjectPrefix: String = ""
         static let forwardMapping: [String: [String]] = [
-            "test@example.com": ["test@example2.com"]
+            "john@example.com": ["john@example2.com"],
+            "bill@example.com": ["bill@example2.com"],
+            "admin@example.com": ["bill@example2.com", "john@example2.com"],
         ]
         static let s3Bucket: String = "example"
         static let s3KeyPrefix: String = "temp/email/"
